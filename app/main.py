6e945aabac957e5c640057fdd69e8df9853c2e6a
Moses Mendoza <mendoza.moses@gmail.com>
2022-03-25 11:58:26 -0700

    Produce to the wordcount output topic
    This takes the calculated tax and sends (produces) it to the output kafka topic
    
    Signed-off-by: Moses Mendoza <mendoza.moses@gmail.com>

--- app/main.py
+++ app/main.py
@@ -3,19 +3,22 @@ import faust
 from .models.sale import Sale
 from decimal import Decimal
 
-TOPIC = 'streams-plaintext-input'
+SALES_TOPIC_NAME = 'streams-plaintext-input'
+TAX_TOPIC_NAME = 'streams-wordcount-output'
 APP_NAME = 'taxservice'
 BROKER = 'kafka://localhost:9092'
 
-app = faust.App(APP_NAME,
-                broker=BROKER)
+app = faust.App(APP_NAME, broker=BROKER)
+sales_topic = app.topic(SALES_TOPIC_NAME, value_type=Sale)
+tax_topic = app.topic(TAX_TOPIC_NAME, value_type=Decimal)
 
-@app.agent(TOPIC, value_type=Sale)
+@app.agent(sales_topic)
 async def read_sale(sales):
   async for sale in sales:
     tax = sale.calculateTax()
     print(f'Sale for {str(sale.amount)}')
     print(f'Tax is {str(tax)}')
+    await tax_topic.send(key="tax", value=tax)
 
 if __name__ == '__main__':
     app.main()
